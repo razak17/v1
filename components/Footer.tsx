8ad95f7b31571d6e85d6a1db23ed217123b15580
Razak <moabdulrazak17@gmail.com>
2022-08-13 14:41:42 +0000

    refactor(Footer): segregate sections for easy readability

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,70 +1,74 @@
 import styles from '../styles/Footer.module.css';
 import mixins from '../styles/Mixins.module.css';
-import IconGitHub from '../components/icons/github';
-import IconTwitter from './icons/twitter';
-import IconLinkedin from './icons/linkedin';
+import Icon from './icons/icon';
+
+const Me = () => (
+	<div>
+		<h2>Razak Mo</h2>
+		<p className={mixins.text_transparent}>Software Developer</p>
+		<ul className={styles.social_list}>
+			{['GitHub', 'Twitter', 'Linkedin'].map((item) => (
+				<li key={`social-${item}`}>
+					<a href='#' target='_blank' rel='noreferrer'>
+						<Icon name={item} />
+					</a>
+				</li>
+			))}
+		</ul>
+		<p className={styles.copy_text}>&copy; 2022 Razak Mo</p>
+	</div>
+);
+
+const Pages = () => (
+	<div>
+		<h3>Pages</h3>
+		<ul>
+			{['home', 'about', 'work', 'blog'].map((item) => (
+				<li className={styles.footer_item} key={`link-${item}`}>
+					<a className={styles.footer_link} href={`#${item}`}>
+						{item}
+					</a>
+				</li>
+			))}
+		</ul>
+	</div>
+);
+
+const QuickLinks = () => (
+	<div>
+		<h3>Quick Links</h3>
+		<ul>
+			{['blog', 'projects', 'imprint', 'privacy policy'].map((item) => (
+				<li className={styles.footer_item} key={`link-${item}`}>
+					<a className={styles.footer_link} href='#'>
+						{item}
+					</a>
+				</li>
+			))}
+		</ul>
+	</div>
+);
+
+const Contact = () => (
+	<div>
+		<h3>Contact</h3>
+		<p className={`${mixins.text_transparent} ${styles.contact_text}`}>
+			Are you interested in working together?
+		</p>
+		<a href='#' className={`${mixins.btn} ${mixins.btn_cta}`}>
+			Get In touch
+		</a>
+	</div>
+);
 
 const Footer = () => {
 	return (
-		<div
-			className={`${styles.footer} ${mixins.bg_primary} ${mixins.py5} ${styles.container}`}
-		>
+		<div className={`${mixins.bg_primary} ${mixins.py5} ${styles.container}`}>
 			<div className={`${mixins.container} ${styles.footer_grid}`}>
-				<div>
-					<h2>Razak Mo</h2>
-					<p className={mixins.text_transparent}>Software Developer</p>
-					<ul className={styles.social_list}>
-						<li>
-							<a href='#' target='_blank' rel='noreferrer'>
-								<IconGitHub />
-							</a>
-						</li>
-						<li>
-							<a href='#' target='_blank' rel='noreferrer'>
-								<IconTwitter />
-							</a>
-						</li>
-						<li>
-							<a href='#' target='_blank' rel='noreferrer'>
-								<IconLinkedin />
-							</a>
-						</li>
-					</ul>
-					<p className={styles.copy_text}>&copy; 2022 Razak Mo</p>
-				</div>
-				<div>
-					<h3>Pages</h3>
-					<ul>
-						{['home', 'about', 'work', 'blog'].map((item) => (
-							<li className={styles.footer_item} key={`link-${item}`}>
-								<a className={styles.footer_link} href={`#${item}`}>
-									{item}
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div>
-					<h3>Quick Links</h3>
-					<ul>
-						{['blog', 'projects', 'imprint', 'privacy policy'].map((item) => (
-							<li className={styles.footer_item} key={`link-${item}`}>
-								<a className={styles.footer_link} href='#'>
-									{item}
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div>
-					<h3>Contact</h3>
-					<p className={`${mixins.text_transparent} ${styles.contact_text}`}>
-						Are you interested in working together?
-					</p>
-					<a href='#' className={`${mixins.btn} ${mixins.btn_cta}`}>
-						Get In touch
-					</a>
-				</div>
+				<Me />
+				<Pages />
+				<QuickLinks />
+				<Contact />
 			</div>
 		</div>
 	);
---
+++ components/icons/icon.tsx
@@ -0,0 +1,13 @@
+import IconExternal from './external';
+import IconGitHub from './github';
+import IconLinkedin from './linkedin';
+import IconTwitter from './twitter';
+
+const Icon = (props: { name: string }) => {
+	if (props.name === 'GitHub') return <IconGitHub />;
+	if (props.name === 'Twitter') return <IconTwitter />;
+	if (props.name === 'Linkedin') return <IconLinkedin />;
+	return <IconExternal />;
+};
+
+export default Icon;
