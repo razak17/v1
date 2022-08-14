f1f5c20ecdd20ee189b96aa6ff61e30f91ab22dd
Razak <moabdulrazak17@gmail.com>
2022-08-14 09:34:41 +0000

    chore: make Logo clickable

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,27 +2,29 @@ import styles from '../styles/Navbar.module.css';
 import mixins from '../styles/Mixins.module.css';
 
 const Navbar = () => {
-  return (
-    <div className={styles.navbar}>
-      <div className={`${mixins.container} ${styles.nav_flex}`}>
-        <h1 className={styles.logo}>Razak Mo.</h1>
-        <nav>
-          <ul className={styles.nav_links}>
-            {['home', 'about', 'work', 'blog'].map((item) => (
-              <li key={`link-${item}`}>
-                <a className={styles.nav_link} href={`#${item}`}>
-                  {item}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <a href='#' className={`${styles.cta} ${mixins.btn} ${mixins.btn_cta}`}>
-          Contact
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.navbar}>
+			<div className={`${mixins.container} ${styles.nav_flex}`}>
+				<a href='#'>
+					<h1 className={styles.logo}>Razak Mo.</h1>
+				</a>
+				<nav>
+					<ul className={styles.nav_links}>
+						{['home', 'about', 'work', 'blog'].map((item) => (
+							<li key={`link-${item}`}>
+								<a className={styles.nav_link} href={`#${item}`}>
+									{item}
+								</a>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<a href='#' className={`${styles.cta} ${mixins.btn} ${mixins.btn_cta}`}>
+					Contact
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
