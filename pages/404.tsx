11b49b099748b42b2044f5dd0c4e0cdc85586a33
Razak <moabdulrazak17@gmail.com>
2022-10-08 12:48:23 +0000

    chore: format

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,12 +4,12 @@ import { NextSeo } from 'next-seo';
 import styles from '../styles/404.module.css';
 
 const NotFound: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <NextSeo title='404' />
-      <h3>404 | That page does not exist...</h3>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<NextSeo title='404' />
+			<h3>404 | That page does not exist...</h3>
+		</div>
+	);
 };
 
 export default NotFound;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,12 @@ import SEO from '../next-seo.config';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<DefaultSeo {...SEO} />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
